4f6b1a42d754b6c415699ecf24813032b45057f1
jonahhess <jonahhessdev@gmail.com>
2025-07-08 17:10:12 +0300

    solved

---
+++ module2/javascript-basics/object-exercise2/main.js
@@ -0,0 +1,9 @@
+const library = {
+  books: [
+    { title: "harry potter", author: "j.k. rowling" },
+    { title: "lord of the rings", author: "j.r.r. tolkien" },
+    { title: "game of thrones", author: "george r.r martin" },
+    { title: "twilight", author: "stephenie meyer" },
+    { title: "the hunger games", author: "suzanne collins" },
+  ],
+};
