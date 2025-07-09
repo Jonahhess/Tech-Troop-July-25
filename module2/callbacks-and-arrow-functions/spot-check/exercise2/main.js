0f0a631b3f661281eedeeeed61b30fd6d5739cf8
jonahhess <jonahhessdev@gmail.com>
2025-07-09 16:19:07 +0300

    solved

---
+++ module2/callbacks-and-arrow-functions/spot-check/exercise2/main.js
@@ -0,0 +1,37 @@
+// let users = [];
+
+// const getData = function () {
+//   setTimeout(function () {
+//     users = [{ name: "Rick" }, { name: "Morty" }];
+//     console.log("Got users");
+//   }, 3000);
+// };
+
+// const displayData = function () {
+//   console.log("Going to display: " + users);
+//   for (const user of users) {
+//     console.log(user.name);
+//   }
+// };
+
+// getData();
+// displayData();
+
+let users = [];
+
+const getData = function (callback) {
+  setTimeout(function () {
+    users = [{ name: "Rick" }, { name: "Morty" }];
+    console.log("Got users");
+    callback();
+  }, 3000);
+};
+
+const displayData = function () {
+  console.log("Going to display: " + users);
+  for (const user of users) {
+    console.log(user.name);
+  }
+};
+
+getData(displayData);
