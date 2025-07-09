507983551d8c05dff8fc0b78b23a3a0388f71596
jonahhess <jonahhessdev@gmail.com>
2025-07-09 14:06:43 +0300

    added variant

--- module2/array-methods/exercise7/main.js
+++ module2/array-methods/exercise7/main.js
@@ -1,9 +1,17 @@
 const { users } = require("../users");
 
-function describeUser(u) {
-  console.log(
-    `${u.name} lives in ${u.address.city}, and owns the company ${u.company.name}`
-  );
+// function describeUser(u) {
+//   console.log(
+//     `${u.name} lives in ${u.address.city}, and owns the company ${u.company.name}`
+//   );
+// }
+
+// users.forEach((u) => describeUser(u));
+
+// variant 2
+
+function describeUser2(u) {
+  return `${u.name} lives in ${u.address.city}, and owns the company ${u.company.name}`;
 }
 
-users.forEach((u) => describeUser(u));
+users.forEach((u) => console.log(describeUser2(u)));
