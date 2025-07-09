21b892452f48bf31790efbd82c49f5caf562aa27
jonahhess <jonahhessdev@gmail.com>
2025-07-09 13:50:25 +0300

    shorter solution

--- module2/array-methods/exercise2/main.js
+++ module2/array-methods/exercise2/main.js
@@ -1,19 +1,22 @@
 const { users } = require("../users");
 
-function partition(users, condition) {
-  const pass = [];
-  const fail = [];
+// function partition(users, condition) {
+//   const pass = [];
+//   const fail = [];
 
-  users.forEach((u) => (condition(u) ? pass.push(u) : fail.push(u)));
+//   users.forEach((u) => (condition(u) ? pass.push(u) : fail.push(u)));
 
-  return { pass, fail };
-}
+//   return { pass, fail };
+// }
 
-function condition(u) {
-  const ret = u.address.zipcode[0] === "5";
-  return ret;
-}
+// function condition(u) {
+//   const ret = u.address.zipcode[0] === "5";
+//   return ret;
+// }
 
-const { pass } = partition(users, condition);
+// const { pass } = partition(users, condition);
+// console.log(pass);
+
+// more specific answer:
+const pass = users.filter((u) => u.address.zipcode[0] === "5");
 console.log(pass);
-// console.log(fail);
