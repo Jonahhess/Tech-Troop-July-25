0843577034d50267483a85364f02a86030742bad
jonahhess <jonahhessdev@gmail.com>
2025-07-09 17:00:38 +0300

    solved

---
+++ module2/callbacks-and-arrow-functions/callback-functions/exercise3/main.js
@@ -0,0 +1,7 @@
+const logData = console.log;
+const displayData = function (alertDataFunc, logDataFunc, data) {
+  alertDataFunc(data);
+  logDataFunc(data);
+};
+
+displayData(console.error, logData, "I like to party");
