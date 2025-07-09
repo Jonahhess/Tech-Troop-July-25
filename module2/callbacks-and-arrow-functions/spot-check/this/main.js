973d5f572ff6a1d9be6b80bdd065716992cba2b0
jonahhess <jonahhessdev@gmail.com>
2025-07-09 16:45:15 +0300

    moved into folder

---
+++ module2/callbacks-and-arrow-functions/spot-check/this/main.js
@@ -0,0 +1,28 @@
+const suspenseBuilder = {
+  name: "El Mysterio",
+  displayName: function () {
+    console.log("You are going to see the name in 3 seconds...");
+
+    setTimeout(function () {
+      //normal function
+      console.log("The name is: " + this.name);
+    }, 3000);
+  },
+};
+
+suspenseBuilder.displayName();
+
+const suspenseBuilder2 = {
+  name: "El Mysterio",
+  displayName: function () {
+    console.log("You are going to see the name in 3 seconds...");
+
+    setTimeout(() => {
+      //this is the change
+      console.log("The name is: " + this.name);
+    }, 3000);
+  },
+};
+
+suspenseBuilder.displayName();
+suspenseBuilder2.displayName();
