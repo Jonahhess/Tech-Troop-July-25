4cc7f498409c103af8c20b4a4b99b36b4d4dbcb3
jonahhess <jonahhessdev@gmail.com>
2025-07-10 17:06:14 +0300

    solved

---
+++ module2/matrices/spot-check/exercise1/main.js
@@ -0,0 +1,10 @@
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+//  rewrite the matrix with the same numbers, but only with 2 columns
+let matrix2 = [[1, 2], [3, 4], [5, 6], [7, 8], [9]];
+
+console.log(matrix2);
