0578203368b038eebaec57a92382316436caf5a3
jonahhess <jonahhessdev@gmail.com>
2025-07-10 23:46:03 +0300

    solved

---
+++ module2/matrices/exercise6/main.js
@@ -0,0 +1,42 @@
+const { Matrix } = require("../exercise2/main.js");
+
+class EmployeeMatrix extends Matrix {
+  loadData(salaryData) {
+    this._matrix = salaryData.map((obj) => [...Object.values(obj)]);
+  }
+
+  getEmployees(department) {
+    return this._matrix
+      .filter((e) => e[2] === department)
+      .map((e) => e[1])
+      .reduce((arr, e) => {
+        arr.push(e);
+        return arr;
+      }, []);
+  }
+
+  getTotalSalary(department) {
+    return this._matrix
+      .filter((e) => e[2] === department)
+      .map((e) => e[3])
+      .reduce((sum, salary) => sum + salary, 0);
+  }
+
+  findRichest() {
+    return this._matrix.reduce((a, b) => (a[3] >= b[3] ? a : b))[1];
+  }
+}
+
+let data = [
+  { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+  { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+  { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+  { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+  { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+  { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 },
+];
+
+let m = new EmployeeMatrix();
+
+m.loadData(data);
+console.log(m.findRichest()); //prints Anisha
