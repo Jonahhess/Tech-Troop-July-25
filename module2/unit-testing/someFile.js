49efeecd71472d1ed2e622222784241304f30209
jonahhess <jonahhessdev@gmail.com>
2025-07-17 09:29:54 +0300

    changed add to arrow function

--- module2/unit-testing/someFile.js
+++ module2/unit-testing/someFile.js
@@ -1,9 +1,5 @@
-const add = function (a, b) {
-  return a + b;
-};
-
+const add = (a, b) => a + b;
 const calculateHyp = (a, b) => Math.sqrt(a * a + b * b);
-
 const removeBugs = (code) => code.filter((c) => c != "BUG");
 
 module.exports = { add, calculateHyp, removeBugs };
