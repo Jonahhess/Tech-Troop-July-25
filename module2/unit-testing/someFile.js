87dd0d75c01cc4571f35716ecb5d5b1f4d341c60
jonahhess <jonahhessdev@gmail.com>
2025-07-17 09:40:39 +0300

    added new class

--- module2/unit-testing/someFile.js
+++ module2/unit-testing/someFile.js
@@ -3,4 +3,27 @@ const calculateHyp = (a, b) => Math.sqrt(a * a + b * b);
 const removeBugs = (code) => code.filter((c) => c != "BUG");
 const clearLowPriority = (objArr) =>
   objArr.filter((obj) => obj.priority === "HIGH");
-module.exports = { add, calculateHyp, removeBugs, clearLowPriority };
+
+class PictureManager {
+  constructor() {
+    this.pictureURLs = [];
+  }
+
+  addPicture(picURL) {
+    this.pictureURLs.push(picURL);
+  }
+
+  removePicture(picURL) {
+    this.pictureURLs.splice(this.pictureURLs.indexOf(picURL), 1);
+  }
+}
+
+module.exports = PictureManager;
+
+module.exports = {
+  add,
+  calculateHyp,
+  removeBugs,
+  clearLowPriority,
+  PictureManager,
+};
