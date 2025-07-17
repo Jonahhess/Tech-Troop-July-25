c2be76327bf1e7f45971d715bfdc8830f5934812
jonahhess <jonahhessdev@gmail.com>
2025-07-17 10:41:43 +0300

    changed toBe(true/false) to toBeTruthy/Falsy

--- module2/unit-testing/Exercises.test.js
+++ module2/unit-testing/Exercises.test.js
@@ -16,3 +16,15 @@ test("should return correct values for ", () => {
   expect(isUndefined).toBe(false);
   expect(tooManyArgs).toBe(false);
 });
+
+test("should return correct values for ", () => {
+  const Ex1 = new Exercises();
+  const isEven = Ex1.isEven(2);
+  const isOdd = Ex1.isEven(1);
+  const isUndefined = Ex1.isEven();
+  const tooManyArgs = Ex1.isEven(undefined, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+  expect(isEven).toBeTruthy();
+  expect(isOdd).toBeFalsy();
+  expect(isUndefined).toBeFalsy();
+  expect(tooManyArgs).toBeFalsy();
+});
