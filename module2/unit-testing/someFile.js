a868a6986e164800aa78f80f0123224d246bcae1
jonahhess <jonahhessdev@gmail.com>
2025-07-17 09:39:19 +0300

    fixed bug as result of test

--- module2/unit-testing/someFile.js
+++ module2/unit-testing/someFile.js
@@ -2,5 +2,5 @@ const add = (a, b) => a + b;
 const calculateHyp = (a, b) => Math.sqrt(a * a + b * b);
 const removeBugs = (code) => code.filter((c) => c != "BUG");
 const clearLowPriority = (objArr) =>
-  obj.filter((obj) => obj.priority === "HIGH");
+  objArr.filter((obj) => obj.priority === "HIGH");
 module.exports = { add, calculateHyp, removeBugs, clearLowPriority };
