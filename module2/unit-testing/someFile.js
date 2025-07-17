7e896a2e6dd0b9504a5be6d8a5f7c2dd0c8b794e
jonahhess <jonahhessdev@gmail.com>
2025-07-17 09:31:29 +0300

    added new fn

--- module2/unit-testing/someFile.js
+++ module2/unit-testing/someFile.js
@@ -1,5 +1,6 @@
 const add = (a, b) => a + b;
 const calculateHyp = (a, b) => Math.sqrt(a * a + b * b);
 const removeBugs = (code) => code.filter((c) => c != "BUG");
-
-module.exports = { add, calculateHyp, removeBugs };
+const clearLowPriority = (objArr) =>
+  obj.filter((obj) => obj.priority === "HIGH");
+module.exports = { add, calculateHyp, removeBugs, clearLowPriority };
