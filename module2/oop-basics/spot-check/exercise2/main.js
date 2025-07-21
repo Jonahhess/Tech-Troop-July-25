82694f06aed365269b8ec6a2be3d675a3afc6448
jonahhess <jonahhessdev@gmail.com>
2025-07-21 09:49:46 +0300

    solved

---
+++ module2/oop-basics/spot-check/exercise2/main.js
@@ -0,0 +1,10 @@
+class Human {
+  constructor(name, age, isFriendly) {
+    this.name = name;
+    this.age = age;
+    this.isFriendly = isFriendly;
+  }
+}
+
+const p1 = new Human("john", 35, "yes");
+console.log(p1);
