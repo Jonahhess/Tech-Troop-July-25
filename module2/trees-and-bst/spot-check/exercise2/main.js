d96285f9e23a0286f40a8cadd98d425bf3672d0f
jonahhess <jonahhessdev@gmail.com>
2025-07-21 12:36:17 +0300

    solved

---
+++ module2/trees-and-bst/spot-check/exercise2/main.js
@@ -0,0 +1,31 @@
+class BSNode {
+  constructor(value) {
+    this.value = value;
+    this.leftChild;
+    this.rightChild;
+  }
+
+  insertNode(newVal) {
+    if (!this.value) {
+      this.value = newVal;
+    } else if (newVal > this.value && this.rightChild) {
+      this.rightChild.insertNode(newVal);
+    } else if (newVal <= this.value && this.leftChild) {
+      this.leftChild.insertNode(newVal);
+    } else if (newVal <= this.value) {
+      this.leftChild = new BSNode(newVal);
+    } else {
+      this.rightChild = new BSNode(newVal);
+    }
+  }
+}
+
+const root = new BSNode("H");
+root.insertNode("E");
+root.insertNode("G");
+root.insertNode("S");
+root.insertNode("L");
+root.insertNode("I");
+root.insertNode("Y");
+
+console.log(root);
