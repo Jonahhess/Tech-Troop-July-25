ec240d1fdced227e237b93883f3aa262fb711164
jonahhess <jonahhessdev@gmail.com>
2025-07-22 09:58:37 +0300

    added example use

--- module2/final-project-trie/main.js
+++ module2/final-project-trie/main.js
@@ -1,3 +1,14 @@
 const Trie = require("./trie.js");
 
 const root = new Trie();
+root.add("apple");
+root.add("app");
+root.add("anaconda");
+root.add("banana");
+root.add("bandana");
+root.add("ban");
+root.remove("coconut");
+root.remove("ana");
+root.remove("app");
+root.getValues("a");
+root.getValues("apple");
