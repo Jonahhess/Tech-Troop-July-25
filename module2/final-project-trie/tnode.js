275d08d8e428354963204035a3fb69c3cf4b0156
jonahhess <jonahhessdev@gmail.com>
2025-07-22 10:37:48 +0300

    removed flag property

--- module2/final-project-trie/tnode.js
+++ module2/final-project-trie/tnode.js
@@ -1,8 +1,46 @@
 class TNode {
-  constructor(value, flag, children) {
+  constructor(value, children) {
     this.value = value;
-    this.flag = flag;
     this.children = children;
+  }
+  longestCommonPrefix(word) {
+    // code stolen from internet and then adapted
+    let commonPrefix = "";
+    const minLength = Math.min(word.length, this.value.length);
+
+    for (let i = 0; i < minLength; i++) {
+      if (word[i] === this.value[i]) {
+        commonPrefix += word[i];
+      } else {
+        break;
+      }
+    }
+    return commonPrefix;
+  }
+
+  addWord(word) {
+    const letter = word[0];
+    if (letter === "") {
+      return true;
+    }
+
+    if (word === this.value) {
+      return false;
+    }
+
+    if (this.value === "" && !(letter in this.children)) {
+      this.children[letter] = new Node(word); // adding entire word as value
+      return true;
+    }
+
+    const prefix = this.longestCommonPrefix(word);
+    if (prefix === this.value) {
+      const restOfWord = word.slice(prefix.length);
+      this.children[restOfWord[0]] = restOfWord;
+      return true;
+    }
+
+    //return this.children[letter].addWord(word.slice[1]);
   }
 }
 
--- module2/final-project-trie/trie.js
+++ module2/final-project-trie/trie.js
@@ -3,7 +3,7 @@ const TNode = require("./tnode.js");
 
 class Trie {
   constructor() {
-    this.root = new TNode("", false, {});
+    this.root = new TNode("", {});
   }
   add(value) {
     return this.root.addWord(value);
