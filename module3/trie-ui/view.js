3418096fae9ff17a998825650b48b9de23528604
jonahhess <jonahhessdev@gmail.com>
2025-07-24 13:44:13 +0300

    generalized

--- module3/trie-ui/view.js
+++ module3/trie-ui/view.js
@@ -2,35 +2,57 @@ import Trie from "./trie.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const model = new Trie();
+  const buttons = document.getElementsByClassName("btn");
+  Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", () => {
+      const opName = button.id;
+      const value = document.getElementById("value-input").value;
+      const status = document.getElementById("status");
 
-  document.getElementById("add-btn").addEventListener("click", () => {
-    const value = document.getElementById("value-input").value;
-    const status = document.getElementById("status");
-    if (value) {
-      model.add(value)
-        ? (status.innerHTML = `added '${value}' to dictionary`)
-        : `'${value}' exists in dictionary`;
-    } else {
-      status.innerHTML = "Status";
-    }
-  });
+      switch (opName) {
+        case "add": {
+          if (model.add(value)) {
+            status.innerHTML = `added '${value}' to dictionary`;
+            document.getElementById("number").innerText =
+              parseInt(document.getElementById("number").innerText) + 1;
+          } else {
+            status.innerHTML = `'${value}' exists in dictionary`;
+          }
+          break;
+        }
+        case "remove": {
+          if (model.remove(value)) {
+            status.innerHTML = `removed '${value}' from dictionary`;
+            document.getElementById("number").innerText =
+              parseInt(document.getElementById("number").innerText) - 1;
+          } else {
+            status.innerHTML = `'${value}' not in dictionary`;
+          }
+          break;
+        }
+        case "find": {
+          if (model.find(value)) {
+            status.innerHTML = `'${value}' exists in dictionary`;
+          } else {
+            status.innerHTML = `'${value}' is not in dictionary`;
+          }
+          break;
+        }
+      }
 
-  document.getElementById("remove-btn").addEventListener("click", () => {
-    const value = document.getElementById("value-input").value;
-    const status = document.getElementById("status");
-    if (value) {
-      model.remove(value)
-        ? (status.innerHTML = `removed '${value}' from dictionary`)
-        : `'${value}' not in dictionary`;
-    } else {
-      status.innerHTML = "Status";
-    }
+      document.getElementById("value-input").value = "";
+      document.getElementById("suggestions").innerHTML = model
+        .getValues()
+        .join(", ");
+    });
   });
 
   document
     .getElementById("autocomplete-input")
-    .addEventListener("keydown", () => {
+    .addEventListener("keyup", () => {
       const value = document.getElementById("autocomplete-input").value;
-      document.getElementById("suggestions").innerHTML = model.getValues(value);
+      document.getElementById("suggestions").innerHTML = model
+        .getValues(value)
+        .join(", ");
     });
 });
