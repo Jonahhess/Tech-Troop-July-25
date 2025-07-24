ec0160bcd0590667d3c78d917a8ac52a9ee89a34
jonahhess <jonahhessdev@gmail.com>
2025-07-24 17:42:32 +0300

    simplified model

--- module3/data-flow/model.js
+++ module3/data-flow/model.js
@@ -1,17 +1,2 @@
-import view from "./view.js";
-
-const handler = {
-  set(target, property, value) {
-    target[property] = value;
-    view.render();
-  },
-  deleteProperty(target, prop) {
-    if (prop in target) {
-      delete target[prop];
-      view.render();
-    }
-  },
-};
-const _model = {};
-const model = new Proxy(_model, handler);
+const model = {};
 export default model;
