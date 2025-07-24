c5634f7e5a1ddbcc2b78b85f56cda74a8c924b05
jonahhess <jonahhessdev@gmail.com>
2025-07-24 12:46:44 +0300

    view partially working

---
+++ module3/trie-ui/view.js
@@ -0,0 +1,49 @@
+import Trie from "./trie.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const model = new Trie();
+
+  document.getElementById("add-btn").addEventListener("click", () => {
+    const value = document.getElementById("value-input").value;
+    const status = document.getElementById("status");
+    if (value) {
+      model.add(value)
+        ? (status.innerHTML = `added '${value}' to dictionary`)
+        : `'${value}' exists in dictionary`;
+    } else {
+      status.innerHTML = "Status";
+    }
+  });
+
+  document.getElementById("remove-btn").addEventListener("click", () => {
+    const value = document.getElementById("value-input").value;
+    const status = document.getElementById("status");
+    if (value) {
+      model.remove(value)
+        ? (status.innerHTML = `removed '${value}' from dictionary`)
+        : `'${value}' not in dictionary`;
+    } else {
+      status.innerHTML = "Status";
+    }
+  });
+
+  document
+    .getElementById("autocomplete-input")
+    .addEventListener("keydown", () => {
+      const value = document.getElementById("autocomplete-input").value;
+      document.getElementById("suggestions").innerHTML = model.getValues(value);
+    });
+});
+
+const printComplete = (prefix, words) =>
+  console.log(`Suggestions for '${prefix}': ${words.join(", ")}`);
+
+const printCannotComplete = (prefix) =>
+  printFailure(`No suggestions found for prefix '${prefix}'`);
+
+const printWordAlreadyExists = (word) =>
+  printFailure(`'${word}' already exists in dictionary`);
+
+const printExit = () => console.log("Goodbye!");
+
+const printRemoved = (word) => printSuccess(`Successfully removed '${word}'`);
