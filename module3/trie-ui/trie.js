c9370f69d3bf7bd405e32eb53dab76d8c9ce0f64
jonahhess <jonahhessdev@gmail.com>
2025-07-24 13:44:23 +0300

    default values

--- module3/trie-ui/trie.js
+++ module3/trie-ui/trie.js
@@ -4,17 +4,17 @@ export default class Trie {
   constructor() {
     this.root = new TNode("");
   }
-  add(value) {
+  add(value = "") {
     return this.root.addWord(value);
   }
-  find(value) {
+  find(value = "") {
     return this.root.findWord(value);
   }
-  getValues(prefix) {
+  getValues(prefix = "") {
     return this.root.predictWords(prefix);
   }
 
-  remove(value) {
+  remove(value = "") {
     return this.root.removeWord(value);
   }
 }
