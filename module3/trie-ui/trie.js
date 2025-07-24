f9339b0789420b59022ebf8f77f7b317c48ca813
jonahhess <jonahhessdev@gmail.com>
2025-07-24 12:06:54 +0300

    changed export

---
+++ module3/trie-ui/trie.js
@@ -0,0 +1,20 @@
+const TNode = require("./tnode.js");
+
+export default class Trie {
+  constructor() {
+    this.root = new TNode("");
+  }
+  add(value) {
+    return this.root.addWord(value);
+  }
+  find(value) {
+    return this.root.findWord(value);
+  }
+  getValues(prefix) {
+    return this.root.predictWords(prefix);
+  }
+
+  remove(value) {
+    return this.root.removeWord(value);
+  }
+}
