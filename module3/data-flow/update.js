a093de4a4b52030225ae2cc5c150d3361dee9156
jonahhess <jonahhessdev@gmail.com>
2025-07-24 18:00:11 +0300

    typo

--- module3/data-flow/update.js
+++ module3/data-flow/update.js
@@ -1,17 +1,17 @@
 import model from "./model.js";
 
 const update = {
-    init: () => {
-        return model;
-    },
-    add: () => (name, text) {
-        model[name] = text;
-        return model;
-    },
-    remove: (name) => {
-        delete model[name];
-        return model;
-    }
-}
+  init: () => {
+    return model;
+  },
+  add: (name, text) => {
+    model[name] = text;
+    return model;
+  },
+  remove: (name) => {
+    delete model[name];
+    return model;
+  },
+};
 
 export default update;
