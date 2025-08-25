ec4ffefa380ac367ea0ede8b0afdf544e98b1615
jonahhess <jonahhessdev@gmail.com>
2025-08-25 17:00:46 +0300

    example server

--- module5/server-intro/server.js
+++ module5/server-intro/server.js
@@ -1,12 +1,41 @@
 const http = require("http");
 
-const server = http.createServer(function (request, response) {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.write("Hello, world");
-  response.end();
+const server = http.createServer(async (req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+
+  if (req.method !== "GET") {
+    res.statusCode = 404;
+    res.write("404 - Page not found");
+  } else {
+    res.statusCode = 200;
+    if (req.url === "/") {
+      res.write("Welcome to my website!");
+    } else if (req.url === "/about") {
+      res.write("This is the about page");
+    } else if (req.url === "/contact") {
+      res.write("Contact me at: jonahhessdev@gmail.com");
+    } else {
+      res.statusCode = 400;
+      res.write(JSON.stringify({ error: "body must include content prop" }));
+    }
+  }
+  res.end();
 });
 
-const port = 3000;
-server.listen(port, function () {
-  console.log(`Node server created at port ${port}`);
+server.listen(8080, () => {
+  console.log("server is listening...");
 });
+
+function readBody(req) {
+  return new Promise((resolve, reject) => {
+    let body = [];
+    req
+      .on("data", (chunk) => {
+        body.push(chunk);
+      })
+      .on("end", () => {
+        body = Buffer.concat(body).toString();
+        resolve(JSON.parse(body));
+      });
+  });
+}
