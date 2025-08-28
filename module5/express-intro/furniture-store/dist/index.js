08ba3536501c9ef932f1823209a0f1f6fdb50930
jonahhess <jonahhessdev@gmail.com>
2025-08-28 16:47:34 +0300

    added two buttons

--- module5/express-intro/furniture-store/dist/index.js
+++ module5/express-intro/furniture-store/dist/index.js
@@ -1,7 +1,24 @@
 addEventListener("DOMContentLoaded", () => {
-  const searchInput = document.getElementById("item-input");
-  const search = document.getElementById("search-button");
-  search.onclick = () => {
-    document.location.href = `/priceCheck/${searchInput.value}`;
+  const itemInput = document.getElementById("item-input");
+  const priceCheck = document.getElementById("price-check-button");
+  const buyButton = document.getElementById("buy-button");
+
+  priceCheck.onclick = async () => {
+    const response = await fetch(`/priceCheck/${itemInput.value}`);
+    const data = await response.json();
+    const result = document.getElementById("result");
+    result.innerHTML = `price: ${data.price}`;
+  };
+
+  buyButton.onclick = async () => {
+    const response = await fetch(`/buy/${itemInput.value}`);
+    const result = document.getElementById("result");
+
+    if (response.status === 400) {
+      result.innerHTML = `Error: could not buy ${itemInput.value}`;
+      return;
+    }
+    const data = await response.json();
+    result.innerHTML = `Congratulations, you've just bought ${data.name} for ${data.price}. There are ${data.inventory} left now in the store.`;
   };
 });
